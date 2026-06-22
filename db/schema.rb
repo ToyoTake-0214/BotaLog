47dfa0d4c1eebb9f6971164a06641162c959f0d2
Toyoda Takeru <takerun20040214@gmail.com>
2026-06-22 23:53:06 +0900

    add:Gemi18n

--- db/schema.rb
+++ db/schema.rb
@@ -39,12 +39,13 @@ ActiveRecord::Schema[7.2].define(version: 2026_06_21_041734) do
 
   create_table "users", force: :cascade do |t|
     t.string "user_name", null: false
-    t.string "email"
+    t.string "email", null: false
     t.string "password_digest", null: false
-    t.string "first_name"
-    t.string "last_name"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
