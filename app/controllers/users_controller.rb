94efba4688ae7c98fe8aa8ab21de6f8735ee1198
Toyoda Takeru <takerun20040214@gmail.com>
2026-06-27 16:24:47 +0900

    add:flash_message,uiを整理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,16 +7,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      session[:user_id] = @user.id
+      redirect_to root_path,success: t(".success")
     else
-      render :new
+      flash.now[:danger] = t(".failure")
+      render :new ,status: :unprocessable_entity
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:user_name,:password,:password_confirmation)
+    params.require(:user).permit(:user_name,:password)
   end
 
 end
